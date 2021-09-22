cc2b82139db1fc991720fdf771198a6577774ef3
AP-Porag <smauhp@gmail.com>
2021-09-23 01:29:54 +0600

    Initial commit

---
+++ resources/views/backend/partials/header.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Material Dash</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('assets/backend/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/backend/vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{asset('assets/backend/css/demo/style.css')}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset('assets/backend/images/favicon.png')}}" />
+</head>
+<body>
+<script src="{{asset('assets/backend/js/preloader.js')}}"></script>
---
+++ resources/views/backend/partials/script-link.blade.php
@@ -0,0 +1,14 @@
+<!-- plugins:js -->
+<script src="{{asset('assets/backend/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="{{asset('assets/backend/js/material.js')}}"></script>
+<script src="{{asset('assets/backend/js/misc.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<!-- End custom js for this page-->
+</body>
+</html>
+
